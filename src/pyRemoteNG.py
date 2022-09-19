fb25de1534e03b8ac5ad8460011d5f88ccca1d6e
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-09-19 19:37:38 +0530

    test gui working

--- src/pyRemoteNG.py
+++ src/pyRemoteNG.py
@@ -1,7 +1,15 @@
-#imports
+from tkinter import *
+
+class pyRemoteNG:
+    def __init__(self,app):
+        self.testbutton = ttk.Button(text="Test Button")
+        self.testbutton.pack()
 
 def main():
-    print("Main Begins")
+    root=Tk()
+    root.title("pyRemoteNG")
+    obj = pyRemoteNG(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
