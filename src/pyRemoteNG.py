83a5c6c833477b7ac81fa0ea274851cf82f02031
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-09-20 21:37:59 +0530

    add dummy menu bar

--- src/pyRemoteNG.py
+++ src/pyRemoteNG.py
@@ -2,12 +2,21 @@ from tkinter import *
 
 class pyRemoteNG:
     def __init__(self,app):
-        self.testbutton = ttk.Button(text="Test Button")
+        self.testbutton = Button(text="Test Button")
         self.testbutton.pack()
 
 def main():
     root=Tk()
     root.title("pyRemoteNG")
+    mymenu = Menu(root)
+    root.config(menu=mymenu)
+    myfilemenu = Menu(mymenu, tearoff=False)
+    mymenu.add_cascade(label="File", menu=myfilemenu)
+    myfilemenu.add_command(label="New connection")
+    myfilemenu.add_command(label="Export to file")
+    myfilemenu.add_command(label="SSH File transfer")
+    myfilemenu.add_separator()
+    myfilemenu.add_command(label="Quit", command=lambda: root.quit())
     obj = pyRemoteNG(root)
     root.mainloop()
 
