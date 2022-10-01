7a10c785d2c78a14acd36ed8890358936bc06b28
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-10-01 21:18:35 +0530

    add frames for left panel and terminal

--- src/pyRemoteNG.py
+++ src/pyRemoteNG.py
@@ -2,8 +2,11 @@ from tkinter import *
 
 class pyRemoteNG:
     def __init__(self,app):
-        self.testbutton = Button(text="Test Button")
-        self.testbutton.pack()
+        left_panel = Frame(app, bg='blue', width=300, height=app.winfo_screenheight())
+        left_panel.grid(row=1, column=0, sticky="ns")
+        left_panel.grid_propagate(False) 
+        terminal = Frame(app, bg='yellow', width=app.winfo_screenwidth(), height=app.winfo_screenheight(), padx=3, pady=3)
+        terminal.grid(row=1, column=1, sticky="nsew")
 
 def main():
     root=Tk()
@@ -13,11 +16,13 @@ def main():
     root.config(menu=mymenu)
     myfilemenu = Menu(mymenu, tearoff=False)
     mymenu.add_cascade(label="File", menu=myfilemenu)
-    myfilemenu.add_command(label="New connection")
+    myfilemenu.add_command(label="Add New connection")
+    myfilemenu.add_command(label="Quick Connect")
     myfilemenu.add_command(label="Export to file")
     myfilemenu.add_command(label="SSH File transfer")
     myfilemenu.add_separator()
     myfilemenu.add_command(label="Quit", command=lambda: root.quit())
+    
     obj = pyRemoteNG(root)
     root.mainloop()
 
